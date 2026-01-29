aa25134e828be65d56e261542a000a05147bd546
pandutria <panduadyatama029@gmail.com>
2026-01-29 08:16:13 +0700

    first commit

--- database/migrations/2026_01_29_004929_create_nokos_table.php
+++ database/migrations/2026_01_29_004929_create_nokos_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('nokos_controllers', function (Blueprint $table) {
+        Schema::create('nokos', function (Blueprint $table) {
             $table->id();
             $table->string("service_name");
-            $table->string("service_name");
+            $table->string("price");
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('nokos_controllers');
+        Schema::dropIfExists('nokos');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NokosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -8,7 +9,12 @@ Route::post('/register', [UserController::class, 'register']); //all
 Route::post('/register/admin', [UserController::class, 'registerAdmin']); //admin
 Route::post('/login', [UserController::class, 'login']); //all
 
+Route::post('/transaction/nokos', [NokosController::class, 'index']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [UserController::class, 'me']);
     Route::post('/logout', [UserController::class, 'logout']);
+
+    Route::get('/transaction/nokos/user', [NokosController::class, 'indexByUser']);
+    Route::post('/transaction/nokos/create', [NokosController::class, 'store']);
 });
